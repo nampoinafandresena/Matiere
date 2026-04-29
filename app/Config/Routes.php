ca76f167a5b41decfb5d664d42990332c9683592
Nomena-coder <raobelinanomenjanahary@gmail.com>
2026-04-29 10:57:32 +0300

    Init note model

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,8 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/produits', 'Produit::index');
-$routes->get('/produit/(:num)', 'Produit::show/$1');
-
-$routes->get('/etudiants', 'Etudiant::index');
 
